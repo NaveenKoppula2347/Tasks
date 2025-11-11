d5a3636244dbb8b22d90ef8ea173d5d3a316b47f
NaveenKoppula2347 <naveenkoppula1701@gmail.com>
2025-11-11 17:33:21 +0530

    Add files via upload

--- Javascript Tasks/evenorodd.js
+++ Javascript Tasks/evenorodd.js
@@ -1,1 +1,38 @@
+let output = []
+for (let i = 1; i <= 10; i++) {
+    if (i >= 1) {
+        console.log("Number:", i);
+        output.push(i)
+    }
+
+    if (i % 2 === 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+
+    if (i <= 3) {
+        console.log("Smaller Number");
+    } else if (i <= 7) {
+        console.log("Medium Number");
+    } else {
+        console.log("Large Number");
+    }
+}
+console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
